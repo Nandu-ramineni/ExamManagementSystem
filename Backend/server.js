351f6805b8a1170615456807f9a540697c941c90
Nandu-ramineni <nandurameneni5577@gmail.com>
2025-12-06 23:38:25 +0530

    fix api helath

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
     res.send('You have reached the EMS Backend Server');
 });
 
-app.get('api/health', (req, res) => {
+app.get('/api/health', (req, res) => {
     res.status(200).send('API is healthy');
 }
 )
